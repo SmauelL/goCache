b258ae61b09321488fb2bf61a7adc0ae86928811
SmauelL <ljxlm502661058@163.com>
2020-11-09 18:05:57 +0800

    comment

--- cache.go
+++ cache.go
@@ -6,29 +6,30 @@ import (
 )
 
 type cache struct {
-	mu sync.Mutex
-	lru *lru.Cache
+	mu         sync.Mutex
+	lru        *lru.Cache
 	cacheBytes int64
 }
 
-func (c *cache) add(key string,value ByteView){
+func (c *cache) add(key string, value ByteView) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if c.lru == nil {
-		c.lru = lru.New(c.cacheBytes,nil)
+		c.lru = lru.New(c.cacheBytes, nil)
 	}
-	c.lru.Add(key,value)
+	c.lru.Add(key, value)
 }
 
-func (c *cache) get(key string)(value ByteView,ok bool){
+//get
+func (c *cache) get(key string) (value ByteView, ok bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	if c.lru == nil{
+	if c.lru == nil {
 		return
 	}
 
-	if v, ok := c.lru.Get(key); ok{
-		return v.(ByteView),ok
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
 	}
 	return
 }
