21d542b669c1508616a09a9794f7ef507f588b8b
SmauelL <ljxlm502661058@163.com>
2020-11-10 16:46:58 +0800

    bug fix

--- http.go
+++ http.go
@@ -26,12 +26,11 @@ func (p *HTTPPool) Log(format string, v ...interface{}) {
 }
 
 // ServerHTTP handle all http request
-func (p *HTTPPool) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if !strings.HasPrefix(r.URL.Path, p.basePath) {
 		panic("HTTPPool serving unexpected path: " + r.URL.Path)
 	}
 	p.Log("%s %s", r.Method, r.URL.Path)
-
 	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
 	if len(parts) != 2 {
 		http.Error(w, "bad request", http.StatusBadRequest)
@@ -43,13 +42,13 @@ func (p *HTTPPool) ServerHTTP(w http.ResponseWriter, r *http.Request) {
 
 	group := GetGroup(groupName)
 	if group == nil {
-		http.Error(w, "no such group:"+groupName, http.StatusNotFound)
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
 		return
 	}
 
 	view, err := group.Get(key)
 	if err != nil {
-		http.Error(w,err.Error(),http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
