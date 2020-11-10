443946d4cd2f3fb94f5fb1f4db0b4d74a718174b
SmauelL <ljxlm502661058@163.com>
2020-11-10 16:17:03 +0800

    httpServer

--- http.go
+++ http.go
@@ -1,5 +1,12 @@
 package goCache
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
 const defaultBasePath = "/_goCache/"
 
 type HTTPPool struct {
@@ -12,4 +19,39 @@ func NewHTTPPool(self string) *HTTPPool {
 		self:     self,
 		basePath: defaultBasePath,
 	}
+}
+
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+func (p *HTTPPool) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group:"+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
 }
