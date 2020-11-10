f953ac9f028bd834c5c45d09c2c4fc1c0719c03f
SmauelL <ljxlm502661058@163.com>
2020-11-10 15:44:57 +0800

    http

---
+++ http.go
@@ -0,0 +1,15 @@
+package goCache
+
+const defaultBasePath = "/_goCache/"
+
+type HTTPPool struct {
+	self     string
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
