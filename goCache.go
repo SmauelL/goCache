bad47a676dc684a15a770c242f4059e7d2427d4e
SmauelL <ljxlm502661058@163.com>
2020-11-13 15:41:35 +0800

    gochache update

--- goCache.go
+++ goCache.go
@@ -6,20 +6,25 @@ import (
 	"sync"
 )
 
-type Getter interface {
-	Get(key string) ([]byte, error)
-}
-
-type GetterFunc func(key string) ([]byte, error)
-
-func (f GetterFunc) Get(key string) ([]byte, error) {
-	return f(key)
-}
-
+// A Group is a cache namespace and associated data loaded spread over
 type Group struct {
 	name      string
 	getter    Getter
 	mainCache cache
+	peers     PeerPicker
+}
+
+// A Getter loads data for a key.
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// A GetterFunc implements Getter with a function.
+type GetterFunc func(key string) ([]byte, error)
+
+// Get implements Getter interface function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
 }
 
 var (
@@ -27,6 +32,7 @@ var (
 	groups = make(map[string]*Group)
 )
 
+// NewGroup create a new instance of Group
 func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 	if getter == nil {
 		panic("nil Getter")
@@ -42,6 +48,8 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 	return g
 }
 
+// GetGroup returns the named group previously created with NewGroup, or
+// nil if there's no such group.
 func GetGroup(name string) *Group {
 	mu.RLock()
 	g := groups[name]
@@ -49,32 +57,60 @@ func GetGroup(name string) *Group {
 	return g
 }
 
+// Get value for a key from cache
 func (g *Group) Get(key string) (ByteView, error) {
 	if key == "" {
 		return ByteView{}, fmt.Errorf("key is required")
 	}
 
 	if v, ok := g.mainCache.get(key); ok {
-		log.Println("[GoCache] hit")
+		log.Println("[GeeCache] hit")
 		return v, nil
 	}
+
 	return g.load(key)
 }
 
+// RegisterPeers registers a PeerPicker for choosing remote peer
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
 func (g *Group) load(key string) (value ByteView, err error) {
+	if g.peers != nil {
+		if peer, ok := g.peers.PickPeer(key); ok {
+			if value, err = g.getFromPeer(peer, key); err == nil {
+				return value, nil
+			}
+			log.Println("[GeeCache] Failed to get from peer", err)
+		}
+	}
+
 	return g.getLocally(key)
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
 }
 
 func (g *Group) getLocally(key string) (ByteView, error) {
 	bytes, err := g.getter.Get(key)
 	if err != nil {
 		return ByteView{}, err
+
 	}
 	value := ByteView{b: cloneBytes(bytes)}
 	g.populateCache(key, value)
 	return value, nil
 }
 
-func (g *Group) populateCache(key string, value ByteView) {
-	g.mainCache.add(key, value)
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
 }
--- peers.go
+++ peers.go
@@ -1,9 +1,12 @@
 package goCache
 
+// PeerPicker is the interface that must be implemented to locate
+// the peer that owns a specific key.
 type PeerPicker interface {
-	PickPeer(key string) (peer PeerPicker,ok bool)
+	PickPeer(key string) (peer PeerGetter, ok bool)
 }
 
+// PeerGetter is the interface that must be implemented by a peer.
 type PeerGetter interface {
-	Get(group string,key string)([]byte,error)
+	Get(group string, key string) ([]byte, error)
 }
