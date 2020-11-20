afbb1dc4651356983461ac5fe52b13a2917f6e06
SmauelL <ljxlm502661058@163.com>
2020-11-20 11:43:34 +0800

    protobuf

--- http.go
+++ http.go
@@ -2,7 +2,9 @@ package goCache
 
 import (
 	"fmt"
+	"github.com/golang/protobuf/proto"
 	"goCache/consistenthash"
+	pb "goCache/goCachePb"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -62,37 +64,47 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	body, err := proto.Marshal(&pb.Response{Value: view.ByteSlice()})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/octet-stream")
-	w.Write(view.ByteSlice())
+	w.Write(body)
 }
 
 type httpGetter struct {
 	baseURL string
 }
 
-func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
 	u := fmt.Sprintf(
 		"%v%v/%v",
 		h.baseURL,
-		url.QueryEscape(group),
-		url.QueryEscape(key),
+		url.QueryEscape(in.GetGroup()),
+		url.QueryEscape(in.GetKey()),
 	)
 	res, err := http.Get(u)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("server returned:%v", res.Status)
+		return fmt.Errorf("server returned:%v", res.Status)
 	}
 
 	bytes, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("reading response body:%v", err)
+		return fmt.Errorf("reading response body:%v", err)
 	}
 
-	return bytes, nil
+	if err = proto.Unmarshal(bytes, out); err != nil {
+		return fmt.Errorf("decoding response body:%v", err)
+	}
+
+	return nil
 }
 
 func (p *HTTPPool) Set(peers ...string) {
